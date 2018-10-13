eb825bad386556fd9e3ae8de88e175557ed4c26a
Xiphrin <arnaud.guenard@epitech.eu>
2018-10-13 19:38:29 +0800

    Add files via upload

---
+++ temperature_arduino.ino
@@ -0,0 +1,19 @@
+int ThermistorPin = 0;
+float adc;
+float T;
+float B = 3435.0;
+float R0 = 10000;
+float T0 = 298.15;
+
+void setup() {
+Serial.begin(9600);
+}
+
+void loop() {
+  adc = analogRead(ThermistorPin);
+  T = 1/(log((R0*1024/adc - R0)/R0)/B+1/T0) - 273.15;
+  Serial.print("Temperature: ");
+  Serial.print(T);
+  Serial.println(" °C"); 
+  delay(500);
+}
